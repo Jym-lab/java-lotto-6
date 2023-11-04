9a3ecc91ee26740cead6558e2ccdeb6b3304beac
Jym-lab <jym9809@gmail.com>
2023-11-04 17:45:42 +0900

    test: add userAmount Test

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -22,6 +22,30 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    /*
+    * 여기서부터 로또 구입 금액 테스트
+    * 1. 영어가 포함되었는지
+    * 2. 숫자가 음수인지
+    * 3. 1000으로 나누어 떨어지는지
+    */
+    @DisplayName("구입 금액이 음수인 경우 예외가 발생한다.")
+    @Test
+    void amountIsNegative(){
+        assertThatThrownBy(() -> new UserAmount("-180,000"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("구입 금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void AmountContainsAlphabet(){
+        assertThatThrownBy(() -> new UserAmount("1800ab"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("구입 금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void amountNotDivisibleByThousand(){
+        assertThatThrownBy(() -> new UserAmount("180,100"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     // 아래에 추가 테스트 작성 가능
 }
