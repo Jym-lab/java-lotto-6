158b25b86ece263131f2dc01f5f1ab817e77f59b
Jym-lab <jym9809@gmail.com>
2023-11-06 11:47:38 +0900

    feat: OutputView

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+public class OutputView {
+    private static final String LOTTO_COUNT = "개를 구매했습니다.";
+
+    public static void printLottoCount(int LottoCount){
+        System.out.println(LottoCount + LOTTO_COUNT);
+    }
+
+    public static void printResult() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+}
