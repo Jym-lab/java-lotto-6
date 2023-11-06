51a4a1e01199c02fac6c2aaa93d5f27cd95adecd
Jym-lab <jym9809@gmail.com>
2023-11-06 10:21:28 +0900

    feat: UserInput Type Casting and listSplit

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,22 +2,43 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
     private final static String INPUT_AMOUNT = "구입금액을 입력해 주세요.";
     private final static String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private final static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    public static String inputAmount(){
+    public static int inputAmount() {
         System.out.println(INPUT_AMOUNT);
-        return Console.readLine();
+        return numberCasting(Console.readLine());
     }
 
-    public static String inputWinningNumbers(){
+    public static List<Integer> inputWinningNumbers() {
         System.out.println(INPUT_WINNING_NUMBERS);
-        return Console.readLine();
+        return listSplit(Console.readLine().split(","));
     }
 
-    public static String inputBounsNumber(){
+    public static int inputBounsNumber() {
         System.out.println(INPUT_BONUS_NUMBER);
-        return Console.readLine();
+        return numberCasting(Console.readLine());
+    }
+
+    private static int numberCasting(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e){
+            ErrorMessage.NOT_A_NUMBER.print();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static List<Integer> listSplit (String[] numbers) {
+        List<Integer> ret = new ArrayList<>();
+        for (String number : numbers) {
+            ret.add(numberCasting(number));
+        }
+        return ret;
     }
 }
