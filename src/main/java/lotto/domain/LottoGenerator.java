58a744f21202d79cfc5bbbbdc29f677a3970c816
Jym-lab <jym9809@gmail.com>
2023-11-06 11:41:30 +0900

    feat: LottoGenerator

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+public class LottoGenerator {
+    private final static int LOTTO_MIN_NUMBER = 1;
+    private final static int LOTTO_MAX_NUMBER = 45;
+    private final static int LOTTO_NUMBER_COUNT = 6;
+    public static List<Integer> generate(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange
+                (LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_COUNT);
+        Collections.sort(numbers);
+        return numbers;
+    }
+}
