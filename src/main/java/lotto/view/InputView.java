7cef20f38764bf482f912a7478cdc155762edb2b
Jym-lab <jym9809@gmail.com>
2023-11-07 15:00:17 +0900

    fix: InputView Re-enter on Exception

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -12,18 +12,32 @@ public class InputView {
     private final static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public static int inputAmount() {
-        System.out.println(INPUT_AMOUNT);
-        return numberCasting(Console.readLine());
+        try {
+            System.out.println(INPUT_AMOUNT);
+            return numberCasting(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            return inputAmount();
+        }
     }
 
     public static List<Integer> inputWinningNumbers() {
-        System.out.println(INPUT_WINNING_NUMBERS);
-        return listSplit(Console.readLine().split(","));
+        try {
+            System.out.println(INPUT_WINNING_NUMBERS);
+            return listSplit(Console.readLine().split(","));
+        } catch (IllegalArgumentException e) {
+            return inputWinningNumbers();
+        }
+
     }
 
-    public static int inputBounsNumber() {
-        System.out.println(INPUT_BONUS_NUMBER);
-        return numberCasting(Console.readLine());
+    public static int inputBonusNumber() {
+        try {
+            System.out.println(INPUT_BONUS_NUMBER);
+            return numberCasting(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            return inputBonusNumber();
+        }
+
     }
 
     private static int numberCasting(String number) {
