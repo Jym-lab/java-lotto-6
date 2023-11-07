1ace8ea0c964b49eff856fd439678dc49285eb8c
Jym-lab <jym9809@gmail.com>
2023-11-07 14:59:20 +0900

    fix: LottoGenerator UnsupportedOperationException fix

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,8 +12,8 @@ public class LottoGenerator {
     private final static int LOTTO_NUMBER_COUNT = 6;
 
     public static List<Integer> generate() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange
-                (LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_COUNT);
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange
+                (LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_COUNT));
         Collections.sort(numbers);
         return numbers;
     }
